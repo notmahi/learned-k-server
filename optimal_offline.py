39a764b12ebe10cd16b6328eca9db550b8ac56c2
Nur Muhammad "Mahi" Shafiullah <n.m.mahi@gmail.com>
2019-04-04 15:51:28 -0400

    Update optimal offline to get server assignments
    Added new method to get the server assignment schedule as well.

--- optimal_offline.py
+++ optimal_offline.py
@@ -12,10 +12,11 @@ class KServer:
         """
         self.graph = None
         self.BigNumber = (1<<30) # Maximum weight it is able to handle
-        self.dist_scaling = 1. * 10**5
+        self.dist_scaling = 1. * 10**6
         self.set_metric(order)
         self.add_servers(servers)
         self.add_requests(requests)
+        self.serve = None
         if servers is not None and requests is not None:
             if len(servers) > len(requests):
                 import warnings
@@ -61,6 +62,39 @@ class KServer:
 
     def _optimal_cost(self):
         return (self.graph.OptimalCost() + (len(self.requests) * self.BigNumber)) / self.dist_scaling
+      
+    def get_serves(self, verbose=False):
+        """
+        Find and print all the flow arcs
+        """
+        if self.serve is not None:
+            return self.serve
+        graph = self.graph
+        self.arcs = []
+        for i in range(graph.NumArcs()):
+            cost = graph.Flow(i) * graph.UnitCost(i)
+            if graph.Flow(i) > 0:
+                if verbose:
+                    print('%5s -> %5s' % (
+                        graph.Tail(i),
+                        graph.Head(i)))
+                self.arcs.append((graph.Tail(i),
+                                 graph.Head(i)))
+        self.arcs = sorted(self.arcs)
+        self.serve = [0] * len(self.requests)
+        for (source, sink) in self.arcs:
+            if source == 0 or sink in self.server_nodes:
+                continue
+            elif sink in self.request_nodes:
+                if source in self.server_nodes:
+                    server = source - 1
+                else:
+                    server = self.serve[(source - 2 - len(self.servers)) // 2]
+                self.serve[(sink - 1 - len(self.servers)) // 2] = server
+        if verbose:
+            print(self.serve)
+        return self.serve
+                    
 
     def _build_graph(self):
         self.graph = pywrapgraph.SimpleMinCostFlow()
@@ -68,8 +102,10 @@ class KServer:
         S = len(self.servers)
         R = len(self.requests)
         server_nodes = range(1, S + 1)
+        self.server_nodes = server_nodes
         # for every request, there are two nodes
         request_nodes = range(S + 1, S + 2 * R + 1, 2)
+        self.request_nodes = request_nodes
         end_node = S + 2 * R + 1
 
         # take a node number, and return the server location
